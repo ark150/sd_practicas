3b1bdfb7e143bf2e590243adabc81ebe94c1e4ac
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-04-10 01:32:02 -0500

    Prueba región crítica

--- src/algoritmosextra/AlgoritmosExtra.java
+++ src/algoritmosextra/AlgoritmosExtra.java
@@ -5,6 +5,14 @@
  */
 package algoritmosextra;
 
+import static java.lang.Thread.currentThread;
+
+import java.awt.Color;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  *
  * @author Samuel
@@ -14,8 +22,40 @@ public class AlgoritmosExtra {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         // TODO code application logic here
+        RegionCritica rc = new RegionCritica();
+        // ExecutorService service = Executors.newSingleThreadExecutor();
+        // Future<Boolean> f1 = service.submit(rc);
+        // System.out.println(f1.get());
+        System.out.println(rc.isInUse());
+
+        // rc.serve();
+        System.out.println("Cambia a verde");
+        rc.asignaRecurso(Color.GREEN);
+        System.out.println(rc.isInUse());
+        if (!rc.isInUse()) {
+            rc.asignaRecurso(Color.RED);
+            System.out.println("Está libre");
+        } else {
+            rc.asignaRecurso(Color.PINK);
+            System.out.println("Está ocupado");
+        }
+
+        try {
+            currentThread().sleep(5500);
+            if(!rc.isInUse())
+            {
+                System.out.println("Se liberó");
+                rc.asignaRecurso(Color.YELLOW);
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("Ocurrio un error");
+            System.out.println(e.getMessage());
+        }
+
+        // service.shutdown();
     }
     
 }
